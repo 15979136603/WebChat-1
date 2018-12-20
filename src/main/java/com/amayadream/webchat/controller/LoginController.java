fee04ca258a7742d8d871b57e7a4e745f258b410
MrJiangzemin <36179251+MrJiangzemin@users.noreply.github.com>
2018-12-20 16:17:16 +0800

    add register function

--- src/main/java/com/amayadream/webchat/controller/LoginController.java
+++ src/main/java/com/amayadream/webchat/controller/LoginController.java
@@ -7,14 +7,19 @@ import com.amayadream.webchat.utils.CommonDate;
 import com.amayadream.webchat.utils.LogUtil;
 import com.amayadream.webchat.utils.NetUtil;
 import com.amayadream.webchat.utils.WordDefined;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Author :  Amayadream
@@ -24,6 +29,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping(value = "/user")
 public class LoginController {
+
 
     @Resource
     private IUserService userService;
@@ -70,5 +76,37 @@ public class LoginController {
         session.removeAttribute("login_status");
         attributes.addFlashAttribute("message", defined.LOGOUT_SUCCESS);
         return "redirect:/user/login";
+    }
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("userPassword") String userPassword,
+                           @RequestParam("userPhone") String age,
+                           @RequestParam("userEmail") String userEmail,
+                           @RequestParam("truesex") String sex, Model model) {
+        String pro="upload/";
+        pro+=username+"/"+username+".jpg";
+        Date day=new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        User user = new User();
+        user.setProfilehead(pro);
+        user.setFirsttime(df.format(day));
+        user.setNickname(username);
+        user.setUserid(username);
+        user.setStatus(1);
+        user.setPassword(userPassword);
+        user.setAge(Integer.parseInt((age)));
+        user.setEmail(userEmail);
+        user.setSex(Integer.parseInt(sex));
+        if(userService.selectUserByUserid(username)!= null) {
+            model.addAttribute("mes","用户名重复");
+            return "register";
+        };
+        userService.insert(user);
+        return "login";
+    }
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public String register(){
+        return "register";
     }
 }
