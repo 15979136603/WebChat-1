eae2cabffe21546b0f05e8f4e9f0501c22bb0fd8
JackieHuang <757423677@qq.com>
2018-12-23 17:05:17 +0800

    Merge branch 'master' of https://github.com/Jackie-Hung/WebChat-1
    # Conflicts:
    #	test/com/amayadream/webchat/dao/IUserDaoTest.java

--- src/main/java/com/amayadream/webchat/controller/LoginController.java
+++ src/main/java/com/amayadream/webchat/controller/LoginController.java
@@ -73,7 +73,7 @@ public class LoginController {
                 }
             }
         }
-
+    }
     @RequestMapping(value = "/logout")
     public String logout(HttpSession session, RedirectAttributes attributes, WordDefined defined) {
         session.removeAttribute("userid");
